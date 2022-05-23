a68f0383b7f85a63d861cabb0754056a1432dfe4
abdul ansari <abdul.tanveer@gmail.com>
2022-05-23 14:26:23 +0530

    recyclerview part-1

--- app/src/main/java/com/example/myapplication/RecyclerActivity.java
+++ app/src/main/java/com/example/myapplication/RecyclerActivity.java
@@ -1,14 +1,17 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class RecyclerActivity extends AppCompatActivity {
+    String[] subjects = new String[]{"english","hindi","urdu","english","hindi","urdu"}; //plug
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler);
+        RecyclerView subjectRecyclerView = findViewById(R.id.subs_recyclerView); //socket
     }
 }
