00e5d23b624361e1a8e560375b39f7c31c69e078
abdul ansari <abdul.tanveer@gmail.com>
2022-05-26 15:46:43 +0530

    adding and removing the fragment dynamically

--- app/src/main/java/com/example/myapplication/NewsActivity.java
+++ app/src/main/java/com/example/myapplication/NewsActivity.java
@@ -1,30 +1,27 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.myapplication.fragments.HeadlinesFragment;
 import com.example.myapplication.fragments.NewsFragment;
 
 public class NewsActivity extends AppCompatActivity implements HeadlinesFragment.HeadLinesSelectListener {
-
+    HeadlinesFragment headlinesFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
-        //on the tablet both the fragments would be there
-        //but on the phone there won't be any fragment
-        //check if the app has been launched on a phone or a tablet..
-              //if news fragment is not null then the app is on tablet else its on phone
-        if(getSupportFragmentManager().findFragmentById(R.id.fragmentNews) == null) {
-            HeadlinesFragment headlinesFragment = HeadlinesFragment.newInstance("one", "one"); //creating a fragment dynamically
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.fragment_container, headlinesFragment, "Headlinesfragmentphone")
-                    .commit(); //adding fragment on the fly/dynammically
-        }
+        headlinesFragment = HeadlinesFragment.newInstance("one", "one"); //creating a fragment dynamically
+
+
+
     }
 
     @Override
@@ -33,5 +30,43 @@ public class NewsActivity extends AppCompatActivity implements HeadlinesFragment
            //if news fragment is not null then the app is on tablet else its on phone
         NewsFragment newsFragment = (NewsFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentNews);
         newsFragment.setText(headline);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         super.onCreateOptionsMenu(menu);
+         getMenuInflater().inflate(R.menu.news_menu,menu);
+         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+         super.onOptionsItemSelected(item);
+         switch (item.getItemId()){
+             case R.id.add_frag_menu:
+                 addHeadlinesFragment();
+                 break;
+             case R.id.remove_frag:
+                 removeHeadLinesFragment();
+                 break;
+         }
+        return true;
+
+    }
+
+    private void removeHeadLinesFragment() {
+        getSupportFragmentManager().beginTransaction().remove(headlinesFragment).commit();
+    }
+
+    private void addHeadlinesFragment() {
+        //on the tablet both the fragments would be there
+        //but on the phone there won't be any fragment
+        //check if the app has been launched on a phone or a tablet..
+        //if news fragment is not null then the app is on tablet else its on phone
+        if(getSupportFragmentManager().findFragmentById(R.id.fragmentNews) == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, headlinesFragment, "Headlinesfragmentphone")
+                    .commit(); //adding fragment on the fly/dynammically
+        }
     }
 }
