c218075ed79bd1c4bec2f551890b859b50c4b73b
abdul ansari <abdul.tanveer@gmail.com>
2022-05-26 13:32:59 +0530

    created a newsactivity and 2 fragments

---
+++ app/src/main/java/com/example/myapplication/fragments/NewsFragment.java
@@ -0,0 +1,35 @@
+package com.example.myapplication.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapplication.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link NewsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NewsFragment extends Fragment {
+
+
+    public NewsFragment() {
+        // Required empty public constructor
+    }
+
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_news, container, false);
+    }
+}
