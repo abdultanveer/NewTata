a29724c184620f1b6d0ebc31d1c216c6b00d501d
abdul ansari <abdul.tanveer@gmail.com>
2022-05-26 13:59:44 +0530

    passing data from  NewsActivity to NewsFragment n setting it on textview

--- app/src/main/java/com/example/myapplication/fragments/NewsFragment.java
+++ app/src/main/java/com/example/myapplication/fragments/NewsFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
@@ -17,7 +18,7 @@ import com.example.myapplication.R;
  * create an instance of this fragment.
  */
 public class NewsFragment extends Fragment {
-
+TextView tvNews;
 
     public NewsFragment() {
         // Required empty public constructor
@@ -31,11 +32,14 @@ public class NewsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false);
+        View fragment = inflater.inflate(R.layout.fragment_news, container, false);
+        tvNews = fragment.findViewById(R.id.tvNews);
+        return fragment;
     }
 
     public void setText(String headline) {
         Toast.makeText(getActivity(), "newsfrag-- "+headline, Toast.LENGTH_SHORT).show();
+        tvNews.setText(headline);
 
     }
 }
