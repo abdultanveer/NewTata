f38d65cff11516bfc462549cfd65973f93d1cb9b
abdul ansari <abdul.tanveer@gmail.com>
2022-05-26 13:40:10 +0530

    headline item selected listener

--- app/src/main/java/com/example/myapplication/fragments/HeadlinesFragment.java
+++ app/src/main/java/com/example/myapplication/fragments/HeadlinesFragment.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.myapplication.R;
 
@@ -15,7 +18,8 @@ import com.example.myapplication.R;
  * Use the {@link HeadlinesFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HeadlinesFragment extends Fragment {
+public class HeadlinesFragment extends Fragment implements AdapterView.OnItemClickListener {
+    ListView headlinesLv;
 
     // TODO: Rename parameter arguments, choose names that match
 
@@ -32,6 +36,15 @@ public class HeadlinesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_headlines, container, false);
+        View fragment = inflater.inflate(R.layout.fragment_headlines, container, false);
+        headlinesLv = fragment.findViewById(R.id.lvHeadlines);
+        headlinesLv.setOnItemClickListener(this);
+        return fragment;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+        String rowText = adapterView.getItemAtPosition(position).toString();
+        Toast.makeText(getActivity(), rowText, Toast.LENGTH_SHORT).show();
     }
 }
